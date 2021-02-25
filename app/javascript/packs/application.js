92e7db953c549a9dc69f4745c925fe7f5f62284c
Aman Patel <patel1wq@uwindsor.ca>
2021-02-24 20:44:03 -0500

    added react-rails

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// Support component names relative to this directory:
+console.log("Hello world, form webpack")
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
