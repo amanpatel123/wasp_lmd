7823ead5e49049cec14e419b353e4c4c7f13bac3
Prakort <leanprakort@gmail.com>
2021-02-28 15:06:18 -0500

    switch bg variant nav to dark

--- app/javascript/components/Header/Header.jsx
+++ app/javascript/components/Header/Header.jsx
@@ -7,29 +7,42 @@ import './Header.scss';
 const Header = ({hideLinks = false}) => {
   console.log("hidelinks", hideLinks)
   return (
-    <div className="header">
-      <Navbar collapseOnSelect expand="sm">
-        <Navbar.Brand as={Link} to="/">Last Mile Delivery</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" /> 
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-          </Nav> 
-          {
-            !hideLinks ?
+    <div className={ !hideLinks ? "header" : "header-dark" }>
+    {
+      !hideLinks ? 
+        <Navbar collapseOnSelect expand="sm">
+          <Navbar.Brand as={Link} to="/">Last Mile Delivery</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" /> 
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+            </Nav> 
+              <Nav>
+                <Nav.Link as={Link} to="/signup">Research</Nav.Link>
+                <Nav.Link as={Link} to="/signup">Contact Us</Nav.Link>
+                <Nav.Link as={Link} to="/signup">
+                  Log In
+                </Nav.Link>
+              </Nav>
+              {/* <Nav>
+                <Nav.Link as={Link} to="/" className="close"> </Nav.Link>
+              </Nav> */}
+          </Navbar.Collapse>
+        </Navbar>
+
+      :
+        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+          <Navbar.Brand as={Link} to="/">Last Mile Delivery</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" /> 
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+            </Nav> 
             <Nav>
-              <Nav.Link as={Link} to="/signup">Research</Nav.Link>
-              <Nav.Link as={Link} to="/signup">Contact Us</Nav.Link>
-              <Nav.Link as={Link} to="/signup">
-                Log In
-              </Nav.Link>
+              <Nav.Link as={Link} to="/">Back</Nav.Link>
             </Nav>
-            :
-            <Nav>
-              <Nav.Link as={Link} to="/" className="close"> </Nav.Link>
-            </Nav>
-          }
-        </Navbar.Collapse>
-      </Navbar>
+          </Navbar.Collapse>
+        </Navbar>
+    }
+
     </div>
   )
 }
--- app/javascript/components/SignUp/SignUp.jsx
+++ app/javascript/components/SignUp/SignUp.jsx
@@ -1,15 +1,86 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
+import React, { useState }from 'react'
+import { Container, Form, Col, Row, InputGroup } from 'react-bootstrap';
 import { Header } from '../Header';
 
+import './SignUp.scss';
+
 const SignUp = () => {
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <div>
       <Header hideLinks={true} />
 
-      <Container>
-        <h1>Sign up</h1>
-      </Container>
+      <div className="main-content">
+        <Container className="Container justify-content-md-center">   
+          <div className="title">
+            <h1>Register</h1> 
+          </div>    
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Row>
+              <Form.Group as={Col} xs={12} controlId="validationCustom01">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="John"
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} xs={12} controlId="validationCustom01">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Wick"
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} xs={12} controlId="validationCustom01">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  required
+                  type="email"
+                  placeholder="me@gmail.com"
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} xs={12} controlId="validationCustom01">
+                <Form.Label>Confirm Email</Form.Label>
+                <Form.Control
+                  required
+                  type="email"
+                  placeholder="me@gmail.com"
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} xs={12} controlId="validationCustom01">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control
+                  required
+                  type="number"
+                  placeholder="255-255-2555"
+                />
+              </Form.Group>
+            </Form.Row>
+          </Form>
+        </Container>
+      </div>
 
     </div>
   )
