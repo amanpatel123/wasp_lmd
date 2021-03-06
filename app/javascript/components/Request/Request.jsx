fda6dc28ee4271314839c6972590c68507c30a27
Prakort <leanprakort@gmail.com>
2021-03-06 12:33:46 -0500

    remove request fake form

--- app/javascript/components/Request/Request.jsx
+++ app/javascript/components/Request/Request.jsx
@@ -75,7 +75,7 @@ const Request = () => {
                 />
                 <Form.Control.Feedback type="invalid">{errors.fullName}</Form.Control.Feedback>
               </Form.Group>
-
+    
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>Company Name</Form.Label>
                 <Form.Control
@@ -88,6 +88,7 @@ const Request = () => {
                 <Form.Control.Feedback type="invalid">{errors.company}</Form.Control.Feedback>
               </Form.Group>
             </Form.Row>
+            
             <Form.Row className="justify-content-md-center">
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>Email Address</Form.Label>
@@ -101,6 +102,7 @@ const Request = () => {
                 />
                 <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
               </Form.Group>
+
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>Phone Number</Form.Label>
                 <Form.Control
@@ -114,6 +116,7 @@ const Request = () => {
                 <Form.Control.Feedback type="invalid">{errors.phone}</Form.Control.Feedback>
               </Form.Group>
             </Form.Row>
+
             <Form.Row className="justify-content-md-center">
               <Form.Group as={Col} xs={12} md={8}>
                 <Form.Label>Ship From: Address</Form.Label>
@@ -128,7 +131,6 @@ const Request = () => {
                 <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
               </Form.Group>
             </Form.Row>
-           
 
             <Form.Row className="justify-content-md-center">
               <Form.Group as={Col} xs={12} md={4}>
@@ -143,6 +145,7 @@ const Request = () => {
                 />
                 <Form.Control.Feedback type="invalid">{errors.streetNumber}</Form.Control.Feedback>
               </Form.Group>
+
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>Street Name</Form.Label>
                 <Form.Control
@@ -156,6 +159,7 @@ const Request = () => {
                 <Form.Control.Feedback type="invalid">{errors.streetName}</Form.Control.Feedback>
               </Form.Group>
             </Form.Row>
+
             <Form.Row className="justify-content-md-center">
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>City</Form.Label>
@@ -169,6 +173,7 @@ const Request = () => {
                 />
                 <Form.Control.Feedback type="invalid">{errors.city}</Form.Control.Feedback>
               </Form.Group>
+
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>Province</Form.Label>
                 <Form.Control
@@ -182,6 +187,7 @@ const Request = () => {
                 <Form.Control.Feedback type="invalid">{errors.province}</Form.Control.Feedback>
               </Form.Group>
             </Form.Row>
+
             <Form.Row className="justify-content-md-center">
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>Country</Form.Label>
@@ -200,6 +206,7 @@ const Request = () => {
                 </Form.Control>
                 <Form.Control.Feedback type="invalid">{errors.country}</Form.Control.Feedback>
               </Form.Group>
+
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>Postal Code</Form.Label>
                 <Form.Control
@@ -213,6 +220,7 @@ const Request = () => {
                 <Form.Control.Feedback type="invalid">{errors.postalCode}</Form.Control.Feedback>
               </Form.Group>
             </Form.Row>
+
             {/* Ship to address */}
             <Form.Row className="justify-content-md-center">
               <Form.Group as={Col} xs={12} md={8}>
@@ -242,6 +250,7 @@ const Request = () => {
                 />
                 <Form.Control.Feedback type="invalid">{errors.streetNumber}</Form.Control.Feedback>
               </Form.Group>
+
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>Street Name</Form.Label>
                 <Form.Control
@@ -255,6 +264,7 @@ const Request = () => {
                 <Form.Control.Feedback type="invalid">{errors.streetName}</Form.Control.Feedback>
               </Form.Group>
             </Form.Row>
+
             <Form.Row className="justify-content-md-center">
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>City</Form.Label>
@@ -268,6 +278,7 @@ const Request = () => {
                 />
                 <Form.Control.Feedback type="invalid">{errors.city}</Form.Control.Feedback>
               </Form.Group>
+
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>Province</Form.Label>
                 <Form.Control
@@ -281,6 +292,7 @@ const Request = () => {
                 <Form.Control.Feedback type="invalid">{errors.province}</Form.Control.Feedback>
               </Form.Group>
             </Form.Row>
+
             <Form.Row className="justify-content-md-center">
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>Country</Form.Label>
@@ -299,6 +311,7 @@ const Request = () => {
                 </Form.Control>
                 <Form.Control.Feedback type="invalid">{errors.country}</Form.Control.Feedback>
               </Form.Group>
+
               <Form.Group as={Col} xs={12} md={4}>
                 <Form.Label>Postal Code</Form.Label>
                 <Form.Control
@@ -313,73 +326,11 @@ const Request = () => {
               </Form.Group>
             </Form.Row>
 
-
-
-            {/* <Form.Row className="justify-content-md-center">
-              <Form.Group as={Col} xs={12} md={4}>
-                <Form.Label>Industry</Form.Label>
-                <Form.Control
-                  className="industry"
-                  as="select"
-                  type="text"
-                  name="industry"
-                  value={form.industry}
-                  onChange={updateForm}
-                  isInvalid={errors && errors.industry}
-                >
-                  <option>Choose...</option>
-                  <option>Beverage</option>
-                  <option>Building / Construction</option>
-                  <option>Cannabis</option>
-                  <option>Catering</option>
-                  <option>Concierge</option>
-                  <option>Courier</option>
-                  <option>Desserts / Novelties</option>
-                  <option>Field Service</option>
-                  <option>Flowers / Gifts</option>
-                  <option>Grocery / Produce</option>
-                  <option>Laundry / Drycleaning</option>
-                  <option>Moving / Storage</option>
-                  <option>Pharmacy / Medical</option>
-                  <option>Prepared Meals</option>
-                  <option>Restaurant</option>
-                  <option>Retail / eCommerce</option>
-                  <option>Other</option>
-                </Form.Control>
-                <Form.Control.Feedback type="invalid">{errors.country}</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group as={Col} xs={12} md={4}>
-                <Form.Label>Monthly delivery volume</Form.Label>
-                <Form.Control
-                  className="industry"
-                  as="select"
-                  type="text"
-                  name="volume"
-                  value={form.volume}
-                  onChange={updateForm}
-                  isInvalid={errors && errors.volume}
-                >
-                  <option>Choose...</option>
-                  <option>0-20</option>
-                  <option>20-100</option>
-                  <option>100-500</option>
-                  <option>500-2000</option>
-                  <option>2000-10000</option>
-                  <option>20000+</option>
-                </Form.Control>
-                <Form.Control.Feedback type="invalid">{errors.volume}</Form.Control.Feedback>
-              </Form.Group>
-            </Form.Row> */}
-
-
             <Form.Row className="justify-content-md-center">
               <Form.Group as={Col} xs={12} md={8}>
                 <Button className="button" disabled={loading} type="submit">
                   Create A Request
                 </Button>
-                {/* <span className="agreement">
-                  By creating an account, you agree to the Term of Service and Privacy Statment. Let us know if you have any conerns regarding our policies.
-                </span> */}
               </Form.Group>
             </Form.Row>
           </Form>
